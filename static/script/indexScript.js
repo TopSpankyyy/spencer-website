40b0e5deafd86d8e06a3ea3743a1dd31bd859be4
Spencer Clapes <spencerclapes13@gmail.com>
2021-08-07 17:19:07 -0400

    more functionality coming in

---
+++ static/script/indexScript.js
@@ -0,0 +1,26 @@
+window.onload = init;
+
+function domHide(bodyToLoad, bodyToHide) {
+    bodyToHide.style.display = "none";
+    bodyToLoad.style.display = "block";
+}
+
+function init() {
+    var currentVis = document.getElementById("aboutBody");
+    document.getElementById("workBut").addEventListener("click", function() {
+        domHide(document.getElementById("workBody"), currentVis);
+        currentVis = document.getElementById("workBody");
+        });
+    document.getElementById("aboutBut").addEventListener("click", function() {
+        domHide(document.getElementById("aboutBody"), currentVis);
+        currentVis = document.getElementById("aboutBody");
+        });
+    document.getElementById("skillsBut").addEventListener("click", function() {
+        domHide(document.getElementById("skillsBody"), currentVis);
+        currentVis = document.getElementById("skillsBody");
+        });
+    document.getElementById("edBut").addEventListener("click", function() {
+        domHide(document.getElementById("edBody"), currentVis);
+        currentVis = document.getElementById("edBody");
+        });
+}
