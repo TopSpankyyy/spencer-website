c27d8dcb8fe992de21ead519cf7aa1af40133023
Spencer Clapes <spencerclapes13@gmail.com>
2021-08-08 11:35:01 -0400

    cursor on button hover + other changes

--- static/script/indexScript.js
+++ static/script/indexScript.js
@@ -7,26 +7,35 @@ function domHide(bodyToLoad, bodyToHide) {
 
 function init() {
     var currentVis = document.getElementById("aboutBody");
+    var currentBut = document.getElementById("aboutBut");
     document.getElementById("aboutBut").style.background = "#707070";
 
     document.getElementById("workBut").addEventListener("click", function() {
         domHide(document.getElementById("workBody"), currentVis);
+        currentBut.style.background = "#404040";
         currentVis = document.getElementById("workBody");
         document.getElementById("workBut").style.background = "#707070";
+        currentBut = document.getElementById("workBut");
         });
     document.getElementById("aboutBut").addEventListener("click", function() {
         domHide(document.getElementById("aboutBody"), currentVis);
+        currentBut.style.background = "#404040";
         currentVis = document.getElementById("aboutBody");
         document.getElementById("aboutBut").style.background = "#707070";
+        currentBut = document.getElementById("aboutBut");
         });
     document.getElementById("skillsBut").addEventListener("click", function() {
         domHide(document.getElementById("skillsBody"), currentVis);
+        currentBut.style.background = "#404040";
         currentVis = document.getElementById("skillsBody");
         document.getElementById("skillsBut").style.background = "#707070";
+        currentBut = document.getElementById("skillsBut");
         });
     document.getElementById("edBut").addEventListener("click", function() {
         domHide(document.getElementById("edBody"), currentVis);
+        currentBut.style.background = "#404040";
         currentVis = document.getElementById("edBody");
         document.getElementById("edBut").style.background = "#707070";
+        currentBut = document.getElementById("edBut");
         });
 }
