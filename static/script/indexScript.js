f457645e85a65a750da8b328657a7bd0edc33349
Spencer Clapes <spencerclapes13@gmail.com>
2021-08-07 23:58:30 -0400

    last of the day

--- static/script/indexScript.js
+++ static/script/indexScript.js
@@ -7,20 +7,26 @@ function domHide(bodyToLoad, bodyToHide) {
 
 function init() {
     var currentVis = document.getElementById("aboutBody");
+    document.getElementById("aboutBut").style.background = "#707070";
+
     document.getElementById("workBut").addEventListener("click", function() {
         domHide(document.getElementById("workBody"), currentVis);
         currentVis = document.getElementById("workBody");
+        document.getElementById("workBut").style.background = "#707070";
         });
     document.getElementById("aboutBut").addEventListener("click", function() {
         domHide(document.getElementById("aboutBody"), currentVis);
         currentVis = document.getElementById("aboutBody");
+        document.getElementById("aboutBut").style.background = "#707070";
         });
     document.getElementById("skillsBut").addEventListener("click", function() {
         domHide(document.getElementById("skillsBody"), currentVis);
         currentVis = document.getElementById("skillsBody");
+        document.getElementById("skillsBut").style.background = "#707070";
         });
     document.getElementById("edBut").addEventListener("click", function() {
         domHide(document.getElementById("edBody"), currentVis);
         currentVis = document.getElementById("edBody");
+        document.getElementById("edBut").style.background = "#707070";
         });
 }
